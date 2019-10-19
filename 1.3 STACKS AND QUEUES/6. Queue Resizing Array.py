04b223ba5c6b4d839b2b9bbda6f4545272364631
Kate <snoblomma@gmail.com>
2019-10-19 21:43:17 +0100

    Queue Resizing Array

---
+++ 1.3 STACKS AND QUEUES/6. Queue Resizing Array.py
@@ -0,0 +1,52 @@
+import unittest
+
+class TestQueueResizingArray(unittest.TestCase):
+    def test_queue(self):
+        s = 'to be or not to - be - - that - - - is'
+        d = s.split(' ')
+        queueResizingArray = QueueResizingArray(len(d))
+        for i in d:
+            if i == '-':
+                queueResizingArray.dequeue()
+            else:
+                queueResizingArray.enqueue(i)
+        self.assertEqual(queueResizingArray.getString(), 'that is')
+
+class QueueResizingArray(object):
+    head = 0
+    tail = 0
+    s = []
+
+    def __init__(self, capacity):
+        self.s = ['']
+    
+    def isEmpty(self):
+        return self.head == 0
+
+    def enqueue(self, item):
+        if self.tail == len(self.s):
+            self.resize(2 * len(self.s))
+        self.s[self.tail] = item
+        self.tail += 1
+    
+    def dequeue(self):
+        self.head -= 1
+        item = self.s[self.head]
+        self.s[self.head] = None
+        return item
+    
+    def getString(self):
+        res = []
+        for i in self.s:
+            if i != None:
+                res.append(i)
+        return ' '.join(res)
+
+    def resize(self, capacity):
+        copy = [None]*capacity
+        for i in range(self.head):
+            copy[i] = self.s[i]
+        self.s = copy
+
+if __name__ == "__main__":
+    unittest.main()
