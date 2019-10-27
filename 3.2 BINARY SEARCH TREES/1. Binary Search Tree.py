67a229ba3ec1e9fef9b9dca98c1904f38201a2a5
Kate <snoblomma@gmail.com>
2019-10-27 15:34:27 +0000

    BST skeleton

---
+++ 3.2 BINARY SEARCH TREES/1. Binary Search Tree.py
@@ -0,0 +1,19 @@
+class Node(object):
+
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+
+class BST(object):
+
+    def __init__(self, ):
+        self.root = self.Node()
+
+    def put(self, key, val):
+        
+
+    def get(self, key):
+
+    def delete(self, key):
+    
+    
